b6f5dae1d22a28969401766ac8bec8464da5afa9
ewi josepha <ewijosepha@gmail.com>
2023-08-16 20:24:09 +0200

    printing a tree

---
+++ script.js
@@ -0,0 +1,22 @@
+//write a program printing a tree
+
+function printTree(height) {
+  let tree = "";
+
+  for (let i = 1; i <= height; i++) {
+    // loop to add spaces`
+    for (let j = height - i; j >= 0; j--) {
+      tree += " ";
+    }
+
+    // loop to add stars
+    for (let k = 1; k <= 2 * i - 1; k++) {
+      tree += "*";
+    }
+
+    tree += "\n";
+  }
+
+  console.log(tree);
+}
+// printTree(10)
